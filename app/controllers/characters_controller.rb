0892062a973d52ed5604f04cd0e016c5d3443f18
Erica Norby <ericanorby@gmail.com>
2017-02-24 13:32:51 -0500

    added edit functionality

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -18,6 +18,18 @@ class CharactersController < ApplicationController
     redirect_to house_character_path(@house,@character)
   end
 
+  def edit
+    @house = House.find(params[:house_id])
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:house_id])
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+    redirect_to house_path(@house)
+  end
+
   private
 
   def character_params
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -18,6 +18,16 @@ class HousesController < ApplicationController
     redirect_to house_path(@house)
   end
 
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    redirect_to house_path(@house)
+  end
+
   private
 
   def house_params
